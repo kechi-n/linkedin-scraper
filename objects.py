67f7618420aa0ce011b1d28b3b98d1fd644d0d83
kech-up <nkechinwudu@gmail.com>
2018-10-26 11:27:20 -0500

    First commit

---
+++ objects.py
@@ -0,0 +1,82 @@
+
+class Institution(object):
+    institution_name = None
+    website = None
+    industry = None
+    type = None
+    headquarters = None
+    company_size = None
+    founded = None
+
+
+    def __init__(self, name=None, website=None, industry=None, type=None, headquarters=None, company_size=None, founded=None):
+        self.name = name
+        self.website = website
+        self.industry = industry
+        self.type = type
+        self.headquarters = headquarters
+        self.company_size = company_size
+        self.founded = founded
+
+class Experience(Institution):
+    from_date = None
+    to_date = None
+    description = None
+    position_title = None
+
+    def __init__(self, from_date = None, to_date = None, description = None, position_title = None):
+        self.from_date = from_date
+        self.to_date = to_date
+        self.description = description
+        self.position_title = position_title
+
+    def __repr__(self):
+        return "{position_title} at {company} from {from_date} to {to_date}".format( from_date = self.from_date, to_date = self.to_date, position_title = self.position_title, company = self.institution_name)
+
+class Skills(object):
+    def __init__(self, skillname = None):
+        self.skillname = skillname
+
+    def __repr__(self):
+        return "{skillname}".format(skillname = self.skillname)
+
+class Interests(object):
+    def __init__(self, interestname = None):
+        self.interestname = interestname
+
+    def __repr__(self):
+        return "{interestname}".format(interestname = self.interestname)
+
+class Education(Institution):
+    from_date = None
+    to_date = None
+    description = None
+    degree = None
+
+    def __init__(self, from_date = None, to_date = None, description = None, degree = None):
+        self.from_date = from_date
+        self.to_date = to_date
+        self.description = description
+        self.degree = degree
+
+    def __repr__(self):
+        return "{degree} at {company} from {from_date} to {to_date}".format( from_date = self.from_date, to_date = self.to_date, degree = self.degree, company = self.institution_name)
+
+class Scraper(object):
+    driver = None
+
+    def is_signed_in(self):
+        try:
+            self.driver.find_element_by_id("profile-nav-item")
+            return True
+        except:
+            pass
+        return False
+
+    def __find_element_by_class_name__(self, class_name):
+        try:
+            self.driver.find_element_by_class_name(class_name)
+            return True
+        except:
+            pass
+        return False
---
+++ person.py
@@ -0,0 +1,194 @@
+import requests
+from lxml import html
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .functions import time_divide
+from .objects import Experience, Education, Skills, Interests, Scraper
+import os
+
+class Person(Scraper):
+    name = None
+    experiences = []
+    educations = []
+    skills = []
+    interests = []
+    also_viewed_urls = []
+    linkedin_url = None
+
+    def __init__(self, linkedin_url = None, name = None, experiences = [], educations = [], skills=[], interests = [], driver = None, get = True, scrape = True):
+        self.linkedin_url = linkedin_url
+        self.name = name
+        self.experiences = experiences
+        self.educations = educations
+        self.skills = skills
+        self.interests = interests
+
+        if driver is None:
+            try:
+                if os.getenv("CHROMEDRIVER") == None:
+                    driver_path = os.path.join(os.path.dirname(__file__), 'drivers/chromedriver')
+                else:
+                    driver_path = os.getenv("CHROMEDRIVER")
+
+                driver = webdriver.Chrome(driver_path)
+            except:
+                driver = webdriver.Chrome()
+
+        if get:
+            driver.get(linkedin_url)
+
+        self.driver = driver
+
+        if scrape:
+            self.scrape()
+
+
+    def add_experience(self, experience):
+        self.experiences.append(experience)
+
+    def add_education(self, education):
+        self.educations.append(education)
+
+    def add_skill(self, skill):
+        self.skills.append(skill)
+
+    def add_interest(self, interest):
+        self.interests.append(interest)
+
+    def scrape(self, close_on_complete = True):
+        if self.is_signed_in():
+            self.scrape_logged_in(close_on_complete = close_on_complete)
+        else:
+            self.scrape_not_logged_in(close_on_complete = close_on_complete)
+
+    def scrape_logged_in(self, close_on_complete = True):
+        driver = self.driver
+        self.name = driver.find_element_by_class_name("pv-top-card-section__name").text
+
+        driver.execute_script("window.scrollTo(0, Math.ceil(document.body.scrollHeight/2));")
+
+        _ = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.ID, "experience-section")))
+
+        # get experience
+        exp = driver.find_element_by_id("experience-section")
+        for position in exp.find_elements_by_class_name("pv-position-entity"):
+            position_title = position.find_element_by_tag_name("h3").text
+            company = position.find_element_by_class_name("pv-entity__secondary-title").text
+
+            try:
+                times = position.find_element_by_class_name("pv-entity__date-range").text
+                from_date, to_date, duration = time_divide(times)
+            except:
+                from_date, to_date = (None, None)
+            experience = Experience( position_title = position_title , from_date = from_date , to_date = to_date)
+            experience.institution_name = company
+            self.add_experience(experience)
+
+        driver.execute_script("window.scrollTo(0, Math.ceil(document.body.scrollHeight/1.5));")
+
+        _ = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.ID, "education-section")))
+
+        # get education
+        edu = driver.find_element_by_id("education-section")
+        for school in edu.find_elements_by_class_name("pv-profile-section__sortable-item"):
+            university = school.find_element_by_class_name("pv-entity__school-name").text
+            degree = None
+            try:
+                degree = school.find_element_by_class_name("pv-entity__degree-name").text
+                times = school.find_element_by_class_name("pv-entity__dates").text
+                from_date, to_date, duration = time_divide(times)
+            except:
+                from_date, to_date = (None, None)
+            education = Education(from_date = from_date, to_date = to_date, degree=degree)
+            education.institution_name = university
+            self.add_education(education)
+
+        driver.execute_script("window.scrollTo(0, Math.ceil(document.body.scrollHeight/2));")
+
+        _ = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "pv-skill-categories-section__top-skills")))
+
+        #get skills
+
+        ski = driver.find_element_by_class_name("pv-skill-categories-section__top-skills")
+        for skillset in ski.find_elements_by_class_name("pv-skill-category-entity__top-skill"):
+            skillname = skillset.find_element_by_class_name("pv-skill-category-entity__name").text
+            skill = Skills(skillname = skillname)
+            self.add_skill(skill)
+
+        driver.execute_script("window.scrollTo(0, Math.ceil(document.body.scrollHeight/5));")
+
+        _ = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "pv-interests-section")))
+
+        #get Interests
+        int = driver.find_element_by_class_name("pv-interests-section")
+        #for inter in int.find_elements_by_class_name("pv-entity__summary-title"):
+        for inter in int.find_elements_by_tag_name("h3"):
+            interestname = inter.find_element_by_class_name("pv-entity__summary-title-text").text
+            interest = Interests(interestname = interestname)
+            self.add_interest(interest)
+
+        if close_on_complete:
+            driver.close()
+
+
+    def scrape_not_logged_in(self, close_on_complete=True, retry_limit = 10):
+        driver = self.driver
+        retry_times = 0
+        while self.is_signed_in() and retry_times <= retry_limit:
+            page = driver.get(self.linkedin_url)
+            retry_times = retry_times + 1
+
+
+        # get name
+        self.name = driver.find_element_by_id("name").text
+
+        # get experience
+        exp = driver.find_element_by_id("experience")
+        for position in exp.find_elements_by_class_name("position"):
+            position_title = position.find_element_by_class_name("item-title").text
+            company = position.find_element_by_class_name("item-subtitle").text
+
+            try:
+                times = position.find_element_by_class_name("date-range").text
+                from_date, to_date, duration = time_divide(times)
+            except:
+                from_date, to_date = (None, None)
+            experience = Experience( position_title = position_title , from_date = from_date , to_date = to_date)
+            experience.institution_name = company
+            self.add_experience(experience)
+
+        # get education
+        edu = driver.find_element_by_id("education")
+        for school in edu.find_elements_by_class_name("school"):
+            university = school.find_element_by_class_name("item-title").text
+            degree = school.find_element_by_class_name("original").text
+            try:
+                times = school.find_element_by_class_name("date-range").text
+                from_date, to_date, duration = time_divide(times)
+            except:
+                from_date, to_date = (None, None)
+            education = Education(from_date = from_date, to_date = to_date, degree=degree)
+            education.institution_name = university
+            self.add_education(education)
+
+        #get skills
+        ski = driver.find_element_by_class_name("pv-skill-categories-section__top-skills")
+        for skillset in ski.find_elements_by_class_name("pv-skill-category-entity__top-skill"):
+            skillname = skillset.find_element_by_class_name("pv-skill-category-entity__name").text
+            skill = Skills(skillname = skillname)
+            self.add_skill(skill)
+
+        #get Interests
+        int = driver.find_element_by_class_name("pv-interests-section")
+        for inter in int.find_elements_by_tag_name("h3"):
+            interestname = inter.find_element_by_class_name("pv-entity__summary-title-text").text
+            interest = Interests(interestname = interestname)
+            self.add_interest(interest)
+
+        if close_on_complete:
+            driver.close()
+
+    def __repr__(self):
+        return "{name}\n\nExperience\n{exp}\n\nEducation\n{edu}\n\nSkills\n{ski}\n\nInterests\n{int}".format(name = self.name, exp = self.experiences, edu = self.educations, ski = self.skills, int = self.interests)
